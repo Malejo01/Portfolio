b2ff95386496dc237beefcd2400641bdb360a314
Malejo01 <lizarragamauroalejandro@gmail.com>
2023-03-07 21:35:41 -0300

    hasta el min 1 30

---
+++ src/components/Proyecto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Proyecto() {
+    return (
+        <div className='page'>
+            <h1 className='heading'>Proyecto</h1>
+            
+        </div>
+    )
+}
+
+export default Proyecto
